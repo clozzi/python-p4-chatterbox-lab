e464f4af9c0872932ccc738aa56672f67064f48b
Chris Lozzi <clozzi12@gmail.com>
2024-01-22 15:50:13 -0500

    message class and get request updated

--- server/app.py
+++ server/app.py
@@ -14,11 +14,39 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET', 'POST'])
+def messages(body="empty", username="bob"):
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.all()]
+        # sorted_messages = sorted(messages, key=lambda message:message.created_at)
+        # response = make_response(
+        #     messages,
+        #     200
+        # )
+        # return response
+        return make_response(messages, 200)
+    
+    elif request.method == 'POST':
+# POST not working still
+        new_message = Message(
+            body = body,
+            username = username
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+
+        response = make_response(
+            new_message_dict,
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
     return ''
 
